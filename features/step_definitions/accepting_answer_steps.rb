19fb33303d506c8b40aa24393b7f441309840716
Michał Szajbe <michal.szajbe@gmail.com>
2014-06-10 20:35:30 +0200

    Added id attributes to divs

--- features/step_definitions/accepting_answer_steps.rb
+++ features/step_definitions/accepting_answer_steps.rb
@@ -1,8 +1,12 @@
 When(/^I accept that answer$/) do
   visit question_path(@answer.question)
-  click_on "Accept"
+  within "#answer-#{@answer.id}" do
+    click_on "Accept"
+  end
 end
 
 Then(/^That answer should be marked as accepted$/) do
-  expect(page).to have_content("Accepted")
+  within "#answer-#{@answer.id}" do
+    expect(page).to have_content("Accepted")
+  end
 end
