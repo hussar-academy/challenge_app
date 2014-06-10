95c52f9c518247cd8cb08a791687dcf6d4de482a
Michał Szajbe <michal.szajbe@gmail.com>
2014-06-10 14:49:23 +0200

    User profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :validatable
 
   has_many :questions
+  has_many :answers
 
   def to_s
     email
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :answers, only: [:create]
   end
 
+  resources :users, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
